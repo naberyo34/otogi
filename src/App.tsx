6553fb6bf5782641b1fd08693c69501bb8a05cc7
ryo.watanabe <naberyo34@gmail.com>
2020-06-01 22:05:31 +0900

    add: クソ雑100D1ができた

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import diceRoll from './services/diceRoll';
 
 const App: React.FC = () => {
+  const [dice, setDice] = useState(0);
+  const handleClick = () => {
+    const result = diceRoll();
+    setDice(result);
+  };
   return (
     <div className="App">
       <h1>otogi ver0.01</h1>
       <p>ダイスロール</p>
-      <button>する</button>
+      <button type="button" onClick={handleClick}>
+        する
+      </button>
+      <p className="">{dice}</p>
     </div>
   );
-}
+};
 
 export default App;
