b01f713a66b510a76fd479c74d1fcbdeb1607be2
ryo.watanabe <ryo.watanabe@nijibox.co.jp>
2020-06-02 15:06:41 +0900

    add: いちほ ダイス汎用化、名前入力をボコボコ実装中

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import { firestore } from './services/firebase';
-import './App.css';
-import diceRoll from './services/diceRoll';
+import React, { useState, useEffect } from "react";
+import { firestore } from "./services/firebase";
+import "./App.css";
+import diceRoll from "./services/diceRoll";
+
+interface displayResult {
+  playerName: string;
+  dice: {
+    single: number[];
+    last: number;
+  };
+}
 
 const App: React.FC = () => {
-  const [dice, setDice] = useState(0);
-  const handleClick = () => {
-    const result = diceRoll();
-    firestore.collection('dice').add({
-      result,
+  const [playerName, setPlayerName] = useState("");
+  const [displayResult, setDisplayResult] = useState<displayResult>({
+    playerName: "",
+    dice: {
+      single: [],
+      last: 0,
+    },
+  });
+
+  // 名前を設定
+  const handleInputPlayerName = (e: any) => {
+    const inputName = e.target.value;
+    setPlayerName(inputName);
+  };
+
+  // ダイスロールボタン
+  const handleDiceRoll = () => {
+    if (!playerName) {
+      alert("名前を入れてくれ");
+      return;
+    }
+
+    const dice = diceRoll(3, 6);
+    firestore.collection("dice").add({
+      result: { playerName, dice },
     });
   };
 
   useEffect(() => {
-    const queryCollection = firestore.collection('dice');
+    const queryCollection = firestore.collection("dice");
     queryCollection.onSnapshot((querySnapshot) => {
       querySnapshot.docChanges().forEach((change) => {
-        if (change.type === 'added') {
-          const result = change.doc.data().result;
-          setDice(result);
+        if (change.type === "added") {
+          const current = change.doc.data();
+          setDisplayResult(current.result);
         }
       });
     });
@@ -27,11 +55,34 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <h1>otogi ver0.01</h1>
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+      <span>D</span>
+      <select>
+        <option value="100">100</option>
+        <option value="10">10</option>
+        <option value="6">6</option>
+        <option value="4">4</option>
+        <option value="3">3</option>
+      </select>
+      <label>
+        あなたの名前:
+        <input type="text" onChange={(e) => handleInputPlayerName(e)} />
+      </label>
       <p>ダイスロール</p>
-      <button type="button" onClick={handleClick}>
+      <button type="button" onClick={handleDiceRoll}>
         する
       </button>
-      <p className="result">{dice}</p>
+      <div>{displayResult.playerName}さんのダイスロール結果: </div>
+      <div className="singleResult">
+        {displayResult.dice.single.map((single) => (
+          <p className="singleResult__num">{single}</p>
+        ))}
+      </div>
+      <p className="result">{displayResult.dice.last}</p>
     </div>
   );
 };
--- src/services/diceRoll.ts
+++ src/services/diceRoll.ts
@@ -1,8 +1,32 @@
-const diceRoll = (): number => {
-  // 1以上100以下の乱数を生成する
-  // Math.randomは0~1の小数を生成し、Math.floorは小数点以下を全て切り捨てる
-  const result = 1 + Math.floor(Math.random() * 100);
-  return result;
+export interface DiceResult {
+  single: number[];
+  last: number;
 }
+
+/**
+ * 任意の面数、任意の個数でダイスロールを行う
+ * @returns result 結果の値
+ * @params count ダイスを何個(何回)振るか ex: 1D100 なら 1
+ * @params size ダイスの面数 ex: 1D100 なら 100
+ */
+const diceRoll = (count: number, size: number): DiceResult => {
+  let roll = 0;
+  let diceResult: DiceResult = { single: [], last: 0 };
+
+  while (roll < count) {
+    // 1以上size以下の乱数を生成し、lastResultに加える
+    // Math.randomは0~1の小数を生成し、Math.floorは小数点以下を全て切り捨てる
+    const currentResult = 1 + Math.floor(Math.random() * size);
+
+    // 今回出た目をcurrentResultにpushし、lastResultに足す
+    diceResult.single.push(currentResult);
+    diceResult.last += currentResult;
+
+    roll++;
+  }
+
+  console.log(diceResult);
+  return diceResult;
+};
 
 export default diceRoll;
