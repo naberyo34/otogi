681827b7f2300e2fd97f71383248b854904540af
ryo.watanabe <naberyo34@gmail.com>
2020-06-28 11:16:55 +0900

    add: basic認証つけた

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
-import * as functions from 'firebase-functions';
+import functions from 'firebase-functions';
+import express from 'express';
+import basicAuth from 'express-basic-auth';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use(
+  basicAuth({
+    users: {
+      admin: 'passw0rd',
+    },
+  })
+);
+
+exports.app = functions.https.onRequest(app);
